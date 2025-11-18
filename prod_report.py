516bd025195a9f3997402e7f4895973037a81926
TimAnderson2024 <trjjinva@gmail.com>
2025-11-18 11:17:28 -0500

    Made config customizable

--- prod_report.py
+++ prod_report.py
@@ -14,14 +14,10 @@ QUERIES = {
     "los_504" : "env:prod status:error @http.status_code:504",
     "los_502" : "env:prod status:error @http.status_code:502"
 }
-configuration = Configuration()
-configuration.api_key["apiKeyAuth"] = os.getenv("DD_API_KEY")
-configuration.api_key["appKeyAuth"] = os.getenv("DD_APP_KEY")
-configuration.server_variables["site"] = os.getenv("DD_SITE", "datadoghq.com")
 
 
-def get_aggregate(query_string):
-    with ApiClient(configuration) as api_client:
+def get_aggregate(config: Configuration, query_string: str):
+    with ApiClient(config) as api_client:
         api_instance = LogsApi(api_client)
         response = api_instance.aggregate_logs(
             body=LogsAggregateRequest(
@@ -41,14 +37,28 @@ def get_aggregate(query_string):
         if response.data.buckets and len(response.data.buckets) > 0:
             return response.data.buckets[0].computes.get('c0', 0)
         return 0
+    
+def get_all_errors(config: Configuration):
+    # Get ULP data
 
-def get_all_errors():
     for metric, query_string in QUERIES.items():
-        count = get_aggregate(query_string)
+        count = get_aggregate(config, query_string)
         print(f"Total {metric} errors: {count}")
 
+
+def get_config(api_key: str, app_key: str):
+    ddconfig = Configuration()
+    ddconfig.server_variables["site"] = os.getenv("DD_SITE", "datadoghq.com")
+
+    ddconfig.api_key["apiKeyAuth"] = api_key
+    ddconfig.api_key["appKeyAuth"] = app_key
+
+    return ddconfig
+
+
 def main():
-    get_all_errors()
+    los_config = get_config(api_key=os.getenv("DD_LOS_API_KEY"), app_key=os.getenv("DD_LOS_APP_KEY"))
+    get_all_errors(config=los_config)
 
 if __name__ == "__main__":
     main()
