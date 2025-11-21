772007d4c4a0d55ba5780d3ca040e684b6db4fc1
TimAnderson2024 <trjjinva@gmail.com>
2025-11-21 09:33:36 -0500

    Added Jinja templating for MD file generation

--- prod_report.py
+++ prod_report.py
@@ -7,8 +7,8 @@ from datadog_api_client.v2.model.logs_aggregate_request import LogsAggregateRequ
 from datadog_api_client.v2.model.logs_query_filter import LogsQueryFilter
 from datadog_api_client.v2.model.logs_compute import LogsCompute
 from datadog_api_client.v2.model.logs_aggregation_function import LogsAggregationFunction
-
-
+from datetime import datetime
+from jinja2 import Template
 
 def get_aggregate(config: Configuration, query_string: str):
     with ApiClient(config) as api_client:
@@ -32,11 +32,9 @@ def get_aggregate(config: Configuration, query_string: str):
             return response.data.buckets[0].computes.get('c0', 0)
         return 0
 
-
 def get_config(api_key: str, app_key: str):
     ddconfig = Configuration()
-    ddconfig.server_variables["site"] = os.getenv("DD_SITE", "datadoghq.com")
-
+    ddconfig.server_variables["site"] = os.getenv("DD_SITE")
     ddconfig.api_key["apiKeyAuth"] = api_key
     ddconfig.api_key["appKeyAuth"] = app_key
 
@@ -46,19 +44,41 @@ def run_queries():
     with open('queries.json') as f:
         queries_json = json.load(f)
 
+    metrics_dict = {}
     for env in queries_json["environments"]:
+        # Get the correct configuration for the environment
         api_key = os.getenv(env.get("API_KEY"))
         app_key = os.getenv(env.get("APP_KEY"))
         env_config = get_config(api_key=api_key, app_key=app_key)
-
+        
+        # Run each query and store results
         for metric, query_string in env["queries"].items():
-            count = get_aggregate(env_config, query_string)
-            print(f"Total {metric} errors: {count}")
+            metrics_dict[metric] = int(get_aggregate(env_config, query_string))
+    
+    with open('report_template.md') as f:
+        template = Template(f.read())
+
+    output = template.render(
+        date=datetime.today().date(),
+        sba_ulp_504=metrics_dict.get("sba_ulp_504", "None"),
+        sba_ulp_502=metrics_dict.get("sba_ulp_502", "None"),
+        sba_ulp_oom=metrics_dict.get("sba_ulp_oom", "None"),
+        cls_prod_504=metrics_dict.get("sba_cls_504", "None"),
+        cls_prod_502=metrics_dict.get("sba_cls_502", "None"),
+        cls_prod_oom=metrics_dict.get("sba_cls_oom", "None"),
+        filemover_failed=metrics_dict.get("filemover_failed", "None"),
+        los_504=metrics_dict.get("los_502", "None"),
+        los_502=metrics_dict.get("los_504", "None"),
+        osc_synthetic="",
+        osc_failed_backend="",
+        osc_p95=""
+    )
+    print(output)
 
 
 def main():
     load_dotenv()
-    run_queries
+    run_queries()
 
 if __name__ == "__main__":
     main()
---
+++ report_template.md
@@ -0,0 +1,22 @@
+# Daily Infra Report: {{ date }}
+
+## SBA Account
+- **ULP Environment:** 
+    - 504: {{ sba_ulp_504 }}
+    - 502: {{ sba_ulp_502 }}
+    - OOM: {{ sba_ulp_oom }}
+- **CLS-Prod Environment:**
+    - 504: {{ cls_prod_504 }}
+    - 502: {{ cls_prod_502 }}
+    - OOM: {{ cls_prod_oom }}
+- **FileMover Failed Jobs:** {{ filemover_failed }}
+
+## LOS Account
+- 504: {{ los_504 }}
+- 502: {{ los_502 }}
+
+## OSC Account
+- Synthetic Tests: {{ osc_synthetic }}
+- Sentry:
+    - Failed Backend Rates: {{ osc_failed_backend }}
+    - Unusual P95 scores: {{ osc_p95 }}
