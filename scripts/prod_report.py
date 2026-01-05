8421c618a5d3a4e1e81d30c5ed5b045eda9144af
TimAnderson2024 <trjjinva@gmail.com>
2026-01-05 09:28:11 -0500

    Added support for 48h weekend report

--- scripts/prod_report.py
+++ scripts/prod_report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import datetime
 from dotenv import load_dotenv
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.logs_api import LogsApi
@@ -8,30 +9,34 @@ from datadog_api_client.v2.model.logs_aggregate_request import LogsAggregateRequ
 from datadog_api_client.v2.model.logs_query_filter import LogsQueryFilter
 from datadog_api_client.v2.model.logs_compute import LogsCompute
 from datadog_api_client.v2.model.logs_aggregation_function import LogsAggregationFunction
-from datetime import datetime
 from jinja2 import Template
 
 from utils.json_helpers import load_json_from_file
 from utils.query import get_dd_config, get_simple_aggregate
 
-def get_env_data(dd_config: Configuration, queries: dict) -> dict:
+def get_env_data(dd_config: Configuration, queries: dict, timeframe: str) -> dict:
     env_data = {}
+
     for metric, query in queries.items():
-        env_data[metric] = get_simple_aggregate(dd_config, query, "now-24h")
+        env_data[metric] = get_simple_aggregate(dd_config, query, timeframe)
 
     return env_data
 
-def write_report(compiled_data: dict) -> str:
+def write_report(compiled_data: dict, timeframe: str) -> str:
     print(compiled_data)
     with open('templates/report_template.md') as f:
         template = Template(f.read())
 
     output = template.render(
-        date=datetime.today().date(),
+        date=datetime.date.today(),
         data=compiled_data
     )
 
-    output_path = f"{os.getenv('OUTPUT_PATH')}Infra Report {datetime.today().date()}.md"
+    if timeframe == "now-48h":
+        output_path = f"{os.getenv('OUTPUT_PATH')}Weekend Infra Report {datetime.date.today()}.md"
+    else:
+        output_path = f"{os.getenv('OUTPUT_PATH')}Business Day Infra Report {datetime.date.today()}.md"
+
     with open(output_path, 'w') as out_f:
         out_f.write(output)
     
@@ -41,16 +46,21 @@ def create_report():
     json_config = load_json_from_file('config/queries.json')
     env_data = {}
 
+    if datetime.date.today().weekday() == 0:
+        timeframe = "now-48h"
+    else:
+        timeframe = "now-24h"
+  
     for env in json_config.keys():
         env_config, env_queries = json_config[env], json_config[env]["queries"]
         try:
             dd_config = get_dd_config(env_config)
-            env_data[env] =get_env_data(dd_config, env_queries)
+            env_data[env] = get_env_data(dd_config, env_queries, timeframe)
             print(f"Generating report data for {env}... {env_data}")
         except KeyError:
             print(f"Skipping {env} due to missing API keys.")
 
-    out_path = write_report(env_data)
+    out_path = write_report(env_data, timeframe)
     print(f"View report: \n{out_path}")
 
 
