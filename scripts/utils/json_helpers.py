11ece4f396eb32b109073077e9a2b4566288bdbb
TimAnderson2024 <trjjinva@gmail.com>
2026-03-30 12:10:28 -0400

    Deleted unneeded "json_helpers.py"

--- scripts/utils/json_helpers.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_json_from_file(path) -> dict:
-    with open(path) as f:
-        return json.load(f)
-    
-def write_json_to_file(data: dict, filepath: str):
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=4)
