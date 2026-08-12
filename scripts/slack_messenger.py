e917a0aa1c7053c8160bc5ce3518227dd5ae165a
Tim Anderson <tanderson@allocore.com>
2026-08-12 15:38:28 -0400

    Changed syntax for time_period

--- scripts/slack_messenger.py
+++ scripts/slack_messenger.py
@@ -1,5 +1,4 @@
 from env_data import EnvData, Result
-from datetime import date
 from time_utils import eastern_now
 
 class SlackMessenger:
@@ -37,7 +36,7 @@ class SlackMessenger:
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": f"📊 ENV Health Status — {eastern_now().strftime('%Y-%m-%d, %H:%M')}-({time_period})",
+                    "text": f"📊 ENV Health Status — {eastern_now().strftime('%Y-%m-%d, %H:%M')} - {time_period}",
                 },
             }
         )
